b59e97b643ec581a9c5b13f831bd7baa0595fe42
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2026-05-27 17:10:26 -0500

    feat: Configuracion del CROS

--- gestion-solicitudes/src/main/java/co/edu/uniquindio/gestion_solicitudes/config/CorsConfig.java
+++ gestion-solicitudes/src/main/java/co/edu/uniquindio/gestion_solicitudes/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
       @Bean
       public CorsFilter corsFilter() {
             CorsConfiguration config = new CorsConfiguration();
-            config.setAllowedOrigins(List.of("https://proyecto-final-programacion-avanzada-1.onrender.com"));
+            config.setAllowedOrigins(List.of("https://proyecto-final-programacion-avanzada-1.onrender.com", "http://localhost:4200"));
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
             config.setAllowedHeaders(List.of("*"));
             config.setAllowCredentials(true);
